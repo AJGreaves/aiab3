2523083b9686d294ccc88f9e369e7dba425d314c
Anna Greaves <anna.j.greaves@gmail.com>
2024-09-16 18:31:49 +0000

    Display to do tasks and done tasks on home page.

--- taskmanager/settings.py
+++ taskmanager/settings.py
@@ -62,7 +62,7 @@ ROOT_URLCONF = 'taskmanager.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -129,9 +129,21 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+if 'DEV' in os.environ:
+    STATIC_URL = '/static/'
+else:
+    STATIC_URL = '/staticfiles/'
+
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
+
+# CSRF trusted origins
+CSRF_TRUSTED_ORIGINS = [
+    'http://localhost:8000',
+    'https://aiab3-e0e540074e8c.herokuapp.com'
+]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
--- tasks/views.py
+++ tasks/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
-
+from .models import Task
+from datetime import date
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the tasks index.")
+    """ 
+    A view to display all the saved tasks, with the tasks due soonest at the top.
+    """
+    to_do_tasks = Task.objects.filter(completed=False).order_by('due_date')
+    done_tasks = Task.objects.filter(completed=True).order_by('due_date')
+
+    context = {
+        'to_do_tasks': to_do_tasks,
+        'done_tasks': done_tasks,
+        'today': date.today(),
+    }
+
+    return render(request, 'index.html', context)
